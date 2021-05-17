14a7b42f15144257972ff02a6764c346ea28f1bb
Cengiz TORU <cengiztoru@gmail.com>
2021-05-17 23:29:21 +0300

    User Profile Fragment added to screen

--- app/src/main/java/com/cengiztoru/madarchitecturesample/ui/MainActivity.kt
+++ app/src/main/java/com/cengiztoru/madarchitecturesample/ui/MainActivity.kt
@@ -2,11 +2,29 @@ package com.cengiztoru.madarchitecturesample.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.cengiztoru.madarchitecturesample.R
+import com.cengiztoru.madarchitecturesample.databinding.ActivityMainBinding
+import com.cengiztoru.madarchitecturesample.ui.fragments.user_profile.UserProfileFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val userId = "cengiztoru"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        addFragment()
+    }
+
+    private fun addFragment() {
+        supportFragmentManager
+                .beginTransaction()
+                .add(
+                        binding.frmFragmentContainer.id,
+                        UserProfileFragment.newInstance(userId),
+                )
+                .addToBackStack(null)
+                .commit()
     }
 }
