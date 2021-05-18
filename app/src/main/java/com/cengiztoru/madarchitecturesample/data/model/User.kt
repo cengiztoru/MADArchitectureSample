87cdf74c9d9c984c95a4d182fcb1f312d078c151
Cengiz TORU <cengiztoru@gmail.com>
2021-05-18 18:33:50 +0300

    User Model prepared

---
+++ app/src/main/java/com/cengiztoru/madarchitecturesample/data/model/User.kt
@@ -0,0 +1,31 @@
+package com.cengiztoru.madarchitecturesample.data.model
+
+data class User(
+    val avatar_url: String? = null,
+    val blog: String? = null,
+    val created_at: String? = null,
+    val events_url: String? = null,
+    val followers: Int? = null,
+    val followers_url: String? = null,
+    val following: Int? = null,
+    val following_url: String? = null,
+    val gists_url: String? = null,
+    val gravatar_id: String? = null,
+    val html_url: String? = null,
+    val id: Int? = null,
+    val location: String? = null,
+    val login: String? = null,
+    val name: String? = null,
+    val node_id: String? = null,
+    val organizations_url: String? = null,
+    val public_gists: Int? = null,
+    val public_repos: Int? = null,
+    val received_events_url: String? = null,
+    val repos_url: String? = null,
+    val site_admin: Boolean? = null,
+    val starred_url: String? = null,
+    val subscriptions_url: String? = null,
+    val type: String? = null,
+    val updated_at: String? = null,
+    val url: String? = null
+)
