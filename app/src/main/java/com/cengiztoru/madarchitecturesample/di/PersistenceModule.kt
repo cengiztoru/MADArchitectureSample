792e1bfc946733a949f891e080ed4e589dcfa967
Cengiz TORU <cengiztoru@gmail.com>
2021-08-16 23:27:08 +0300

    Database Prepared and Provided

---
+++ app/src/main/java/com/cengiztoru/madarchitecturesample/di/PersistenceModule.kt
@@ -0,0 +1,38 @@
+package com.cengiztoru.madarchitecturesample.di
+
+import android.content.Context
+import androidx.room.Room
+import com.cengiztoru.madarchitecturesample.data.locale.MADArchitectureDatabase
+import com.cengiztoru.madarchitecturesample.util.oneWeekMillis
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Qualifier
+import javax.inject.Singleton
+
+/**
+ * Created by Cengiz TORU on 27/07/2021.
+ * cengiz.toru@tsoft.com.tr
+ */
+
+@Module
+@InstallIn(SingletonComponent::class)
+object PersistenceModule {
+
+    @Provides
+    @Singleton
+    fun provideAppDatabase(
+        @ApplicationContext context: Context,
+    ) = Room.databaseBuilder(
+        context,
+        MADArchitectureDatabase::class.java, "MADArchitectureSample"
+    ).build()
+
+    @Provides
+    fun provideUserDao(
+        db: MADArchitectureDatabase
+    ) = db.userSearchDao()
+
+}
